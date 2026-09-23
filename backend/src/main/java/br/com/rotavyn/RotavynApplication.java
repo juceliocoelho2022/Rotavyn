d439bce6cb312614cf1b30f446a01dc49b7d44b8
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-09-23 08:38:49 -0300

    Implement initial shipment API and CI

---
+++ backend/src/main/java/br/com/rotavyn/RotavynApplication.java
@@ -0,0 +1,9 @@
+package br.com.rotavyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotavynApplication {
+    public static void main(String[] args) { SpringApplication.run(RotavynApplication.class, args); }
+}
